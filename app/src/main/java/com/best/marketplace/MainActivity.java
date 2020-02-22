89bee20c6d14786b2a39311930b2cb2b994949fb
Charles Best <charlesbest8@gmail.com>
2020-02-22 22:36:38 +0100

    updated

---
+++ app/src/main/java/com/best/marketplace/MainActivity.java
@@ -0,0 +1,63 @@
+package com.best.marketplace;
+
+import android.os.Bundle;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.snackbar.Snackbar;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity {
+
+    RecyclerView recyclerView;
+    ProductAdapter adapter;
+    List<Product> productList;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        productList = new ArrayList<>();
+
+        recyclerView = findViewById(R.id.recyclerView);
+        recyclerView.setHasFixedSize(true);
+
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+
+        productList.add(new Product(1,"Accer Laptop", R.drawable.laptop, 1000.0,"Core i7 Accer Laptop"));
+        productList.add(new Product(2,"Robotics Class", R.drawable.robotics, 1000.0,"Become a Robotics Expert"));
+        productList.add(new Product(3,"Programming Class", R.drawable.programming, 2000.0,"Become A Top Notch Software Developer"));
+
+        adapter = new ProductAdapter(this, productList);
+        recyclerView.setAdapter(adapter);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present. .
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
